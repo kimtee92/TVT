832d25a14d0712916f31f17d0c59f16dcca27fe2
kimtee92 <kimtee92@gmail.com>
2018-09-24 14:48:49 +1000

    node setup

--- src/app/shared/globals.ts
+++ src/app/shared/globals.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class Globals {
-  api: string = 'http://localhost:3000';
-  //api: string = 'https://cors-anywhere.herokuapp.com/https://tvt-backend.herokuapp.com';
-  //api: string = 'https://tvt-backend.herokuapp.com';
+  api: string = environment.api
 }
