79eda1288bbab2faa43236f09edf13a8373082c9
Maria Tricia Camille Bilog <Maria.T.Bilog@student.uts.edu.au>
2018-10-03 00:43:06 +1000

    scandriver issueticket ui updated

--- src/app/pages/issueticket/issueticket.component.ts
+++ src/app/pages/issueticket/issueticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 import { TablesDataService } from './tablesData.service';
 
 @Component({
@@ -7,17 +8,28 @@ import { TablesDataService } from './tablesData.service';
   styleUrls: ['./issueticket.component.scss'],
   providers: [TablesDataService]
 })
+
 export class IssueticketComponent implements OnInit {
+  ticketForm: FormGroup;
   tableData: Array<any>;
 
   pageSize = 10;
   pageNumber = 1;
 
-  constructor(private _tablesDataService: TablesDataService) { }
+  constructor(private _tablesDataService: TablesDataService,
+  private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.loadData();
+    this.ticketForm = this.formBuilder.group({
+      violation: [''],
+      fine: [''],
+      remarks: [''],
+    });
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.ticketForm.controls; }
 
   loadData() {
     this.tableData = this._tablesDataService.DATA;
